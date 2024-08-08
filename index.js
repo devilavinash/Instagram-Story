6fb5751fd734650e762eb53893847cdf2901bc58
devilavinash <94991360+devilavinash@users.noreply.github.com>
2024-08-09 01:43:31 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+var arr = [
+  {
+    dp: "https://images.unsplash.com/photo-1719937206158-cad5e6775044?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    story:
+      "https://images.unsplash.com/photo-1711486107626-855a7a5ca7f0?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    dp: "https://images.unsplash.com/photo-1720048171230-c60d162f93a0?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    storyL:
+      "https://images.unsplash.com/photo-1722480403866-fc98f4789f00?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    dp: "https://images.unsplash.com/photo-1722325004266-eb21ffffc1fa?q=80&w=2054&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    story:
+      "https://images.unsplash.com/photo-1721332155567-55d1b12aa271?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    dp: "https://images.unsplash.com/photo-1721999610570-6b6ed05447c7?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    story:
+      "https://plus.unsplash.com/premium_photo-1721858125081-96344964c7a6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw0N3x8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    dp: "https://images.unsplash.com/photo-1720048171180-a8178a198fa8?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    story:
+      "https://images.unsplash.com/photo-1722111059641-be1189421e09?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+];
+var storiyan = document.querySelector(".storiyan")
+var clutter = ""
+arr.forEach(function(elem,id){
+  clutter +=`<div class="story">
+            <img id=${id} src="${elem.dp}" alt="">
+          </div>`
+})
+storiyan.innerHTML = clutter
+storiyan.addEventListener("click",function(event){
+  document.querySelector(".full-screen").style.display = "block"
+  document.querySelector(".full-screen").style.backgroundImage = `url(${arr[event.target.id].story}`
+    //  
+})
